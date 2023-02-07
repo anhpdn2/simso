8e95a6cc582d4d284bbe7be50d42955e52e0fa4e
anhpdn2 <anhpdn2@gmail.com>
2023-02-07 17:18:57 +0700

    ok

---
+++ sim-so-backend/src/test/java/com/ducanh/simsobackend/SimSoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ducanh.simsobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimSoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
