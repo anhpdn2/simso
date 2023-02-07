0568abdc8b5aabbdd8ba7f64635a595d4cd7ea52
AnhPd <anhpdn2@gmail.com>
2023-02-07 21:59:39 +0700

    fix

---
+++ simso-backend/src/main/java/com/ducanh/simsobackend/entity/SIMCard.java
@@ -0,0 +1,109 @@
+package com.ducanh.simsobackend.entity;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@Entity
+@Table(name = "sim_card")
+public class SIMCard {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private UUID id;
+    @Column(name = "type", length = 30)
+    private String type;
+
+    @Column(name = "phone_number", length = 20)
+    private String phoneNumber;
+
+    @Column(name = "price")
+    private Long price;
+
+    @Column(name = "telecom", length = 100)
+    private String telecom;
+
+    @Column(name = "fate")
+    private Integer fate;
+
+    @Column(name = "point")
+    private Integer point;
+
+    @Column(name = "unit_number")
+    private Integer unitNumber;
+
+    @Column(name = "description", length = 600)
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getUnitNumber() {
+        return unitNumber;
+    }
+
+    public void setUnitNumber(Integer unitNumber) {
+        this.unitNumber = unitNumber;
+    }
+
+    public Integer getPoint() {
+        return point;
+    }
+
+    public void setPoint(Integer point) {
+        this.point = point;
+    }
+
+    public Integer getFate() {
+        return fate;
+    }
+
+    public void setFate(Integer fate) {
+        this.fate = fate;
+    }
+
+    public String getTelecom() {
+        return telecom;
+    }
+
+    public void setTelecom(String telecom) {
+        this.telecom = telecom;
+    }
+
+    public Long getPrice() {
+        return price;
+    }
+
+    public void setPrice(Long price) {
+        this.price = price;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+}
---
+++ simso-backend/src/test/java/com/ducanh/simsobackend/SimsoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ducanh.simsobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimsoBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
